733ba9276cacdbd400500e822207acf0eb5a05af
Muhammet Bozkurt <mubozkt@gmail.com>
2024-01-24 10:28:29 +0300

    Flexbox section

--- rn-starter-demos/src/screens/BoxScreen.jsx
+++ rn-starter-demos/src/screens/BoxScreen.jsx
@@ -2,23 +2,52 @@ import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 export default BoxScreen = () => {
   return (
-    <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Child #1</Text>
-      <Text style={styles.textStyle}>Child #2</Text>
-      <Text style={styles.textStyle}>Child #3</Text>
+    <View style={styles.parentStyle}>
+      <Text style={styles.textOneStyle}>Child #1</Text>
+      <Text style={styles.textTwoStyle}>Child #2</Text>
+      <Text style={styles.textThreeStyle}>Child #3</Text>
     </View>
   );
 };
 const styles = StyleSheet.create({
-  viewStyle: {
-    borderWidth: 3,
+  parentStyle: {
+    // borderWidth: 3,
     borderColor: "black",
     flexDirection: 'row',
     height: 200,
-    justifyContent: 'space-around',
+    alignItems: 'flex-start',
+    justifyContent: 'space-between'
+
   },
-  textStyle: {
-    borderWidth: 3,
-    borderColor: "red",
+  textOneStyle: {
+    // borderWidth: 3,
+    // borderColor: "red",
+    // flex: 9,
+    height: 50,
+    width: 50,
+    backgroundColor: 'green',
+    // alignSelf: 'center'
+  },
+  textTwoStyle: {
+    // borderWidth: 3,
+    // borderColor: "red",
+    backgroundColor: 'red',
+    top: 50,
+    // flex: 2,
+    // top: 10,
+    // left: 10,
+    height: 50,
+    width: 50,
+    // ...StyleSheet.absoluteFillObject,
+
+    // position: 'absolute'
+  },
+  textThreeStyle: {
+    // borderWidth: 3,
+    // borderColor: "red",
+    // flex: 3,
+    height: 50,
+    width: 50,
+    backgroundColor: 'blue'
   },
 });
