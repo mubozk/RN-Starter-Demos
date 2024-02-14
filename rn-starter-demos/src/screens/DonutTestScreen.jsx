6dfba62902dc56320d14c7ff51be9f548be7f45c
Muhammet Bozkurt <mubozkt@gmail.com>
2024-02-14 15:46:21 +0300

    functionalize donut

--- rn-starter-demos/src/screens/DonutTestScreen.jsx
+++ rn-starter-demos/src/screens/DonutTestScreen.jsx
@@ -8,42 +8,33 @@ const App = () => {
   const radius = 70;
   const circleCircumference = 2 * Math.PI * radius;
 
-  const leftToSpendAmount = 600;
-  const targetAmount = 1000;
+  const inputArr = [15, 40, 20, 10, 15];
+  const colorsArr = ["#F1FADA", "#9AD0C2", "#2D9596", "#BBC3A4", "#265073"];
+  function calculateDonutPercentage(inputArr) {
+    let cumulativeOffset = 100;
+    return inputArr.map((inputPerc, index) => {
+      const offset =
+        circleCircumference - (circleCircumference * cumulativeOffset) / 100;
+      cumulativeOffset -= inputPerc;
+      return drawCircle(offset, colorsArr[index], (index + 2) * 6);
+    });
+  }
 
-  const spentAmount = targetAmount - leftToSpendAmount;
-  // const percentage = (spentAmount / targetAmount) * 100;
-  const percentage = 30;
-  const strokeDashoffset =
-    circleCircumference - (circleCircumference * percentage) / 100;
-  const strokeDashoffsetFirst = strokeDashoffset / 3;
-
-  const firstValPercentage = 15;
-  const firstDashOffset =
-    circleCircumference - (circleCircumference * 100) / 100;
-
-  const secondValPercentage = 40;
-  const secondDashOffset =
-    circleCircumference - (circleCircumference * 85) / 100;
-
-  const thirdValPercentage = 20;
-  const thirdValOffset = circleCircumference - (circleCircumference * 45) / 100;
-
-  const fourthValPercentage = 10;
-  const fourthValOffset =
-    circleCircumference - (circleCircumference * 25) / 100;
-
-  const fifthValPercentage = 15;
-  const fifthValOffset = circleCircumference - (circleCircumference * 15) / 100;
-
-  const [perc, setPerc] = useState(100);
-
-  function calculateDonutPercentage(inputPerc) {
-    if (inputPerc > perc) {
-      return;
-    }
-    setPerc(perc - inputPerc);
-    const theOffS = circleCircumference - (circleCircumference * perc) / 100;
+  function drawCircle(offset, color, width) {
+    return (
+      <Circle
+        key={color}
+        cx="50%"
+        cy="50%"
+        r={radius}
+        stroke={color}
+        fill="transparent"
+        strokeWidth={width}
+        strokeDasharray={circleCircumference}
+        strokeDashoffset={offset - 5}
+        // strokeLinecap="round"
+      />
+    );
   }
 
   return (
@@ -51,58 +42,7 @@ const App = () => {
       <View style={styles.graphWrapper}>
         <Svg height="300" width="300" viewBox="0 0 180 180">
           <G rotation={-90} originX="90" originY="90">
-            <Circle
-              cx="50%"
-              cy="50%"
-              r={radius}
-              stroke="blue"
-              fill="transparent"
-              strokeWidth="15"
-              strokeDasharray={circleCircumference}
-              strokeDashoffset={firstDashOffset}
-              // strokeLinecap="round"
-            />
-            <Circle
-              cx="50%"
-              cy="50%"
-              r={radius}
-              stroke="grey"
-              fill="transparent"
-              strokeWidth="20"
-              strokeDasharray={circleCircumference}
-              strokeDashoffset={secondDashOffset}
-              // strokeLinecap="round"
-            />
-            <Circle
-              cx="50%"
-              cy="50%"
-              r={radius}
-              stroke="cyan"
-              fill="transparent"
-              strokeWidth="25"
-              strokeDasharray={circleCircumference}
-              strokeDashoffset={thirdValOffset}
-            />
-            <Circle
-              cx="50%"
-              cy="50%"
-              r={radius}
-              stroke="yellow"
-              fill="transparent"
-              strokeWidth="35"
-              strokeDasharray={circleCircumference}
-              strokeDashoffset={fourthValOffset}
-            />
-            <Circle
-              cx="50%"
-              cy="50%"
-              r={radius}
-              stroke="green"
-              fill="transparent"
-              strokeWidth="40"
-              strokeDasharray={circleCircumference}
-              strokeDashoffset={fifthValOffset}
-            />
+            {calculateDonutPercentage(inputArr)}
           </G>
         </Svg>
         <Text style={styles.text}></Text>
@@ -123,11 +63,106 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  text: {
-    position: "absolute",
-    textAlign: "center",
-    fontWeight: "600",
-    fontSize: 18,
-    color: "#394867",
-  },
 });
+
+{
+  /* <Circle
+cx="50%"
+cy="50%"
+r={radius}
+stroke="#F1FADA"
+fill="transparent"
+strokeWidth="15"
+strokeDasharray={circleCircumference}
+strokeDashoffset={firstDashOffset} //
+// strokeLinecap="round"
+/>
+<Circle
+cx="50%"
+cy="50%"
+r={radius}
+stroke="#9AD0C2"
+fill="transparent"
+strokeWidth="20"
+strokeDasharray={circleCircumference}
+strokeDashoffset={secondDashOffset}
+// strokeLinecap="round"
+/>
+<Circle
+cx="50%"
+cy="50%"
+r={radius}
+stroke="#2D9596"
+fill="transparent"
+strokeWidth="25"
+strokeDasharray={circleCircumference}
+strokeDashoffset={thirdValOffset}
+/>
+<Circle
+cx="50%"
+cy="50%"
+r={radius}
+stroke="#BBC3A4"
+fill="transparent"
+strokeWidth="35"
+strokeDasharray={circleCircumference}
+strokeDashoffset={fourthValOffset}
+/>
+<Circle
+cx="50%"
+cy="50%"
+r={radius}
+stroke="#265073"
+fill="transparent"
+strokeWidth="40"
+strokeDasharray={circleCircumference}
+strokeDashoffset={fifthValOffset}
+/> */
+}
+
+// const leftToSpendAmount = 600;
+// const targetAmount = 1000;
+
+// const spentAmount = targetAmount - leftToSpendAmount;
+// const percentage = (spentAmount / targetAmount) * 100;
+// const percentage = 30;
+// const strokeDashoffset =
+//   circleCircumference - (circleCircumference * percentage) / 100;
+
+// const firstValPercentage = 15;
+// const firstDashOffset =
+//   circleCircumference - (circleCircumference * 100) / 100;
+
+// const secondValPercentage = 40;
+// const secondDashOffset =
+//   circleCircumference - (circleCircumference * 85) / 100;
+
+// const thirdValPercentage = 20;
+// const thirdValOffset = circleCircumference - (circleCircumference * 45) / 100;
+
+// const fourthValPercentage = 10;
+// const fourthValOffset =
+//   circleCircumference - (circleCircumference * 25) / 100;
+
+// const fifthValPercentage = 15;
+// const fifthValOffset = circleCircumference - (circleCircumference * 15) / 100;
+
+// text: {
+//   position: "absolute",
+//   textAlign: "center",
+//   fontWeight: "600",
+//   fontSize: 18,
+//   color: "#394867",
+// },
+// function calculateDonutPercentage(inputArr) {
+//   let i = 0;
+//   while (i < inputArr.length) {
+//     const inputPerc = inputArr[i];
+//     if (inputPerc > perc) {
+//       return;
+//     }
+//     setPerc(perc - inputPerc);
+//     const theOffS = circleCircumference - (circleCircumference * perc) / 100;
+//     drawCircle(theOffS);
+//   }
+// }
